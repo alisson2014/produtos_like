92b24ba2a4569f3d6b6507b883b200073a4d3b56
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-04-04 13:27:55 -0300

    refactor: usei o toggle

--- js/viewForm.js
+++ js/viewForm.js
@@ -1,12 +1,6 @@
 const viewButton = document.querySelector("#viewButton");
 const form = document.querySelector("#formCategories");
 
-const addClass = () => {
-  if (form.classList.contains("active")) {
-    form.classList.remove("active");
-  } else {
-    form.classList.add("active");
-  }
-};
-
-viewButton.addEventListener("click", addClass);
+viewButton.addEventListener("click", () => {
+  form.classList.toggle("active");
+});

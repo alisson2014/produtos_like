790ceb4517bc56c8aada5ecd1c34da6e52822976
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-04-04 17:05:07 -0300

    feat: trazendo as categorias4

--- pages/subcategories.php
+++ pages/subcategories.php
@@ -1,5 +1,27 @@
 <div class="categories">
     <h2 class="categories_title">Subcategorias</h2>
+    <?php
+    $sqlCategorias = "SELECT * FROM subcategoria";
+    $consultaCategorias = $pdo->prepare($sqlCategorias);
+    $consultaCategorias->execute();
+
+    while ($dados = $consultaCategorias->fetch(PDO::FETCH_OBJ)) {
+        $name = $dados->nome;
+        $id = $dados->id;
+
+        echo "
+            <div class='categories_table'>
+                <div class='categorie'>
+                    <p>CATEGORIA: {$name}</p>
+                </div>
+                <div class='buttons'>
+                    <button type='button'>Editar</button>
+                    <button type='button'>Deletar</button>
+                </div>
+            </div>
+                ";
+    }
+    ?>
     <button type="button" class="categories_button" id="viewButton">
         Nova subcategoria
     </button>

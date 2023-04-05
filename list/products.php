2fc3fd5b4b7c43f2db3cf20107b09195479e3865
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-04-05 11:35:24 -0300

    style: criei o estilo dos produtos

--- list/products.php
+++ list/products.php
@@ -1,38 +1,42 @@
-<div class="products">
+<div class="products_page">
     <h2 class="title">Produtos</h2>
     <?php
     $sqlProdutos = "SELECT * FROM produto";
     $consultaProdutos = $pdo->prepare($sqlProdutos);
     $consultaProdutos->execute();
-
-    while ($dados = $consultaProdutos->fetch(PDO::FETCH_OBJ)) {
-        $produto = $dados->nome;
-        $valor = $dados->valor;
-        $subcategoria = $dados->subcategoria;
-        $id = $dados->id;
     ?>
-        <div class='categories_list'>
-            <div class='categorie'>
-                <p><?= $subcategoria ?></p>
+    <div class="products_list">
+        <?php
+        while ($dados = $consultaProdutos->fetch(PDO::FETCH_OBJ)) {
+            $produto = $dados->nome;
+            $valor = $dados->valor;
+            $subcategoria = $dados->subcategoria;
+            $id = $dados->id;
+        ?>
+            <div class="products">
+                <div class="product">
+                    <p><?= $produto ?></p>
+                </div>
+                <div class="product">
+                    <p><?= $valor ?></p>
+                </div>
+                <div class="product">
+                    <p><?= $subcategoria ?></p>
+                </div>
+                <div class='buttons'>
+                    <button type='button'>
+                        <a href="index.php?action=register&table=subcategories&id=<?= $id ?>">Editar</a>
+                    </button>
+                    <button type='button'>
+                        <a href="index.php?action=delete&table=subcategories&id=<?= $id ?>">Deletar</a>
+                    </button>
+                </div>
             </div>
-            <div class="product">
-                <p><?= $produto ?></p>
-            </div>
-            <div class="value">
-                <p><?= $valor ?></p>
-            </div>
-            <div class='buttons'>
-                <button type='button'>
-                    <a href="index.php?action=register&table=subcategories&id=<?= $id ?>">Editar</a>
-                </button>
-                <button type='button'>
-                    <a href="index.php?action=delete&table=subcategories&id=<?= $id ?>">Deletar</a>
-                </button>
-            </div>
-        </div>
-    <?php
-    }
-    ?>
+            <hr>
+        <?php
+        }
+        ?>
+    </div>
     <button type="button" class="categories_button">
         Novo produto
     </button>

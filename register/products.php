8a68874ed64f1c15655eb6e00141b7282b6781ae
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-04-05 14:43:54 -0300

    feat: trouxe os dados para o form de produtos

--- register/products.php
+++ register/products.php
@@ -1,13 +1,38 @@
+<?php
+$id = $_GET["id"] ?? NULL;
+
+if (!empty($id)) {
+    $id = (int)$id;
+    $sqlProduto = "SELECT * FROM produto
+        WHERE id = '{$id}' LIMIT 1";
+    $consultaProduto = $pdo->prepare($sqlProduto);
+    $consultaProduto->execute();
+
+    //recuperar os dados do sql
+    $dados = $consultaProduto->fetch(PDO::FETCH_OBJ);
+}
+
+$id = $dados->id ?? NULL;
+$produto = $dados->nome ?? NULL;
+$valor = $dados->valor ?? NULL;
+$categorias_id = $dados->subcategoria ?? NULL;
+?>
+
 <div class="card">
     <h2 class="title">Cadastrar produto</h2>
     <form name="formCadastro" action="?action=save&table=products" method="post" class="form">
         <label for="id" class="form_label">ID:</label>
-        <input type="text" class="form_input" name="id" id="id" value="" readonly>
+        <input type="text" class="form_input" name="id" id="id" value="<?= $id ?>" readonly>
 
         <br>
 
         <label for="produto" class="form_label">Digite o nome do Produto: </label>
-        <input type="text" class="form_input" placeholder="Digite o nome do produto" name="produto" id="produto" value="" required>
+        <input type="text" class="form_input" placeholder="Digite o nome do produto" name="produto" id="produto" value="<?= $produto ?>" required>
+
+        <br>
+
+        <label for="valor" class="form_label">Digite o valor do Produto: </label>
+        <input type="text" class="form_input" placeholder="Digite o valor do produto" name="valor" id="valor" value="<?= $valor ?>" required>
 
         <br>
 

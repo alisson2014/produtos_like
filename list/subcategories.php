69e5967533525b5be1aa93be278ccb3a8479eeec
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-04-05 11:20:33 -0300

    style: ajustes nas subcategorias

--- list/subcategories.php
+++ list/subcategories.php
@@ -1,30 +1,34 @@
-<div class="categories">
+<div class="subcategories">
     <h2 class="title">Subcategorias</h2>
     <?php
     $sqlCategorias = "SELECT * FROM subcategoria";
     $consultaCategorias = $pdo->prepare($sqlCategorias);
     $consultaCategorias->execute();
-
-    while ($dados = $consultaCategorias->fetch(PDO::FETCH_OBJ)) {
-        $nome = $dados->nome;
-        $id = $dados->id;
     ?>
-        <div class='categories_list'>
-            <div class='categorie'>
-                <p><?= $nome ?></p>
+    <div class="categories_list">
+        <?php
+        while ($dados = $consultaCategorias->fetch(PDO::FETCH_OBJ)) {
+            $nome = $dados->nome;
+            $id = $dados->id;
+        ?>
+            <div class='categories'>
+                <div class='categorie'>
+                    <p><?= $nome ?></p>
+                </div>
+                <div class='buttons'>
+                    <button type='button'>
+                        <a href="index.php?action=register&table=subcategories&id=<?= $id ?>">Editar</a>
+                    </button>
+                    <button type='button'>
+                        <a href="index.php?action=delete&table=subcategories&id=<?= $id ?>">Deletar</a>
+                    </button>
+                </div>
             </div>
-            <div class='buttons'>
-                <button type='button'>
-                    <a href="index.php?action=register&table=subcategories&id=<?= $id ?>">Editar</a>
-                </button>
-                <button type='button'>
-                    <a href="index.php?action=delete&table=subcategories&id=<?= $id ?>">Deletar</a>
-                </button>
-            </div>
-        </div>
-    <?php
-    }
-    ?>
+            <hr>
+        <?php
+        }
+        ?>
+    </div>
     <button type="button" class="categories_button">
         <a href="index.php?action=register&table=subcategories">
             Nova categoria

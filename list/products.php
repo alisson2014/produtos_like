41d66091f5c72322d2851e24463f920f1b2bc49a
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-04-05 11:08:27 -0300

    feat: listei alguns produtos

--- list/products.php
+++ list/products.php
@@ -1,5 +1,38 @@
 <div class="products">
     <h2 class="title">Produtos</h2>
+    <?php
+    $sqlProdutos = "SELECT * FROM produto";
+    $consultaProdutos = $pdo->prepare($sqlProdutos);
+    $consultaProdutos->execute();
+
+    while ($dados = $consultaProdutos->fetch(PDO::FETCH_OBJ)) {
+        $produto = $dados->nome;
+        $valor = $dados->valor;
+        $subcategoria = $dados->subcategoria;
+        $id = $dados->id;
+    ?>
+        <div class='categories_list'>
+            <div class='categorie'>
+                <p><?= $subcategoria ?></p>
+            </div>
+            <div class="product">
+                <p><?= $produto ?></p>
+            </div>
+            <div class="value">
+                <p><?= $valor ?></p>
+            </div>
+            <div class='buttons'>
+                <button type='button'>
+                    <a href="index.php?action=register&table=subcategories&id=<?= $id ?>">Editar</a>
+                </button>
+                <button type='button'>
+                    <a href="index.php?action=delete&table=subcategories&id=<?= $id ?>">Deletar</a>
+                </button>
+            </div>
+        </div>
+    <?php
+    }
+    ?>
     <button type="button" class="categories_button">
         Novo produto
     </button>

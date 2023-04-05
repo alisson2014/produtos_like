c7539d1be2eaac435d8671e067841976dbf17bcb
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-04-05 16:52:23 -0300

    feat: criei uma função de retorno

--- pages/error.php
+++ pages/error.php
@@ -1,6 +1,12 @@
 <div class="error">
     <img src="images/404.png" alt="error-404" class="error_img" />
     <button type="button" class="error_button">
-        <a href="index.php?page=home">Voltar</a>
+        <a href="javascript:back()">Voltar</a>
     </button>
 </div>
+
+<script>
+    function back() {
+        history.back();
+    }
+</script>

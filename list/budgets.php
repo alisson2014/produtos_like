2e2f705cc0286450848a1ace9e9e2d4f2fd6ef78
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-04-06 11:22:22 -0300

    feat: passei os dados no update do orcamento

--- list/budgets.php
+++ list/budgets.php
@@ -1,5 +1,40 @@
 <div class="budgets">
     <h2 class="title">Orçamentos</h2>
+    <?php
+    $sqlOrcamentos = "SELECT * FROM orcamento ORDER BY data DESC";
+    $consultaOrcamento = $pdo->prepare($sqlOrcamentos);
+    $consultaOrcamento->execute();
+    ?>
+    <table class="table">
+        <thead class="table_head">
+            <tr class="row">
+                <td class="col">Cliente</td>
+                <td class="col">Data</td>
+                <td class="col actions">Ações</td>
+            </tr>
+        </thead>
+        <tbody class="table_body">
+            <?php
+            while ($dados = $consultaOrcamento->fetch(PDO::FETCH_OBJ)) {
+                $nomeCliente = $dados->nomeCliente;
+                $data = $dados->data;
+                $id = $dados->id;
+            ?>
+                <tr class="row">
+                    <td class="col">
+                        <p><?= $nomeCliente ?></p>
+                    </td>
+                    <td class="col"><?= $data ?></td>
+                    <td class="col buttons">
+                        <button type="button" onclick="registrar('budgets', <?= $id ?>)">Editar</button>
+                        <button type="button" onclick="excluir('budgets', <?= $id ?>)">Excluir</button>
+                    </td>
+                </tr>
+            <?php
+            }
+            ?>
+        </tbody>
+    </table>
     <button type="button" onclick="registrar('budgets')" class="categories_button">
         Novo orçamento
     </button>
--- register/budgets.php
+++ register/budgets.php
@@ -1,18 +1,36 @@
+<?php
+$id = $_GET["id"] ?? NULL;
+
+if (!empty($id)) {
+    $id = (int)$id;
+    $sqlOrcamento = "SELECT * FROM orcamento
+        WHERE id = '{$id}' LIMIT 1";
+    $consultaOrcamento = $pdo->prepare($sqlOrcamento);
+    $consultaOrcamento->execute();
+
+    $dados = $consultaOrcamento->fetch(PDO::FETCH_OBJ);
+}
+
+$id = $dados->id ?? NULL;
+$nomeCliente = $dados->nomeCliente ?? NULL;
+$data = $dados->data ?? NULL;
+?>
+
 <div class="card">
     <h2 class="title">Cadastrar orçamento</h2>
     <form name="formCadastro" action="?action=save&table=budgets" method="post" class="form">
         <label for="id" class="form_label">ID:</label>
-        <input type="text" class="form_input" name="id" id="id" value="" readonly />
+        <input type="text" class="form_input" name="id" id="id" value="<?= $id ?>" readonly />
 
         <br>
 
         <label for="cliente" class="form_label">Digite o nome do cliente: </label>
-        <input type="text" class="form_input" placeholder="Digite o nome do cliente" name="cliente" id="cliente" value="" required />
+        <input type="text" class="form_input" placeholder="Digite o nome do cliente" name="cliente" id="cliente" value="<?= $nomeCliente ?>" required />
 
         <br>
 
         <label for="data" class="form_label">Digite a data do orçamento: </label>
-        <input class="form_select" type="date" id="data" name="data" required />
+        <input class="form_select" type="date" id="data" name="data" value="<?= $data ?>" required />
 
         <br>
 
